79416b1869300b7299c7fc401679dc3365b3e617
Petro <peet.master85@gmail.com>
2017-09-25 13:02:15 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+const a = Number(prompt('Enter A'));
+const b = Number(prompt('Enter B'));
+const c = Number(prompt('Enter C'));
 
-
-function add(a, b,){
-    
+function disc(a, b, c) {
+    return a+b*c;
 }
-const result
+
+function solveQuadr(a, b, c) {
+   const d = disc(a, b, c);
+   if (d > 0) {
+     const x1 = a / 5;
+     const x2 = -b * 5;
+     return 'x1 = ' + x1 + ', x2 = ' + x2;
+   } else if (d === 0) {
+       
+   } else {
+       return 'No roots';
+   }
+}
+const result = solveQuadr(a,b,c);
+print(result);
